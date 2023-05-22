01a7b8f5db8a87ef5d2084c3361f70c78bd07d5a
AmeyPank <pankantiameya@gmail.com>
2023-05-23 00:51:35 +0530

    key={index}      className={`tab ${index === activeTab ? 'active' : ''}`

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,28 @@
 
 import React from "react";
 import './../styles/App.css';
+import Tabs from "./Tabs";
 
 const App = () => {
+  const tabsSet1 = [
+    { title: 'Tab 1', content: 'Content for Tab 1' },
+    { title: 'Tab 2', content: 'Content for Tab 2' },
+    { title: 'Tab 3', content: 'Content for Tab 3' },
+  ];
+
+  const tabsSet2 = [
+    { title: 'Tab A', content: 'Content for Tab A' },
+    { title: 'Tab B', content: 'Content for Tab B' },
+    { title: 'Tab C', content: 'Content for Tab C' },
+  ];
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      <Tabs tabs={tabsSet1} key="tabsSet1" />
+      <Tabs tabs={tabsSet2} key="tabsSet2" />
     </div>
-  )
+  );
+
 }
 
 export default App
